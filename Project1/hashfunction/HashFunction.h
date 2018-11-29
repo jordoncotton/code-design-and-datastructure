4163d8d9f9b3f4c1bc816ff13156714ccd915b1d
Jordon Cotton <s188044@students.aie.edu>
2018-11-29 15:55:06 -0600

    hashing

--- Project1/hashfunction/HashFunction.h
+++ Project1/hashfunction/HashFunction.h
@@ -3,7 +3,13 @@
 
 namespace Hashfunction
 {
+	typedef std::function< unsigned int(const char*, unsigned int)> Hashfunc;
+
+	//implementation of a basic addition hash
 	unsigned int badHash(const char* data, unsigned int length);
 	
-	static Hashfunc;
+	//ADD YOUR FUNCTIONS HERE
+
+	//a helper to access a default hash function
+	static Hashfunc default = badHash;
 }

7e5ecdfb9c71a2338a649b51dde5840d181cf9c4
jordoncotton1 <jordoncotton1@gmail.com>
2018-12-08 15:50:43 -0600

    bubble sorting

--- sorting/Sorting.cpp
+++ sorting/Sorting.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
 
-struct Hero
-{
-	int mHealth = 0;
-
-	bool operator <(Hero something)
-	{
-		return mHealth < something.mHealth;
-	}
-};
-
-Hero lowest(Hero arr[], int size, sometimes)
+//bubble sort from least to greatest
+template <class J>
+void BubbleSorting(J arr[], int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		for (int j = 0; j < size; i++)
+		for (int j = i; j < size; j++)
 		{
-			if (arr[i] < arr[j])
+			if (arr[i] > arr[j])
 			{
-				
+				J temp = arr[i];
 				arr[i] = arr[j];
-
+				arr[j] = temp;
 			}
 		}
 	}
-	return Hero();
 }
 
 int main()
 {
-	Hero something;
-	Hero somethingelse;  
+	int checkArray[8] = { 1, 64, 7, 9, 10, 6, 92, 14 };
+
+	BubbleSorting<int>(checkArray, 8);
+
 }
